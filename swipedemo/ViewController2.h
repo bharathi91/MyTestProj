dae30107319077dec706e45ef831fd8ce20525e0
Shridhara <shridhara.venkataramulu@costrategix.com>
2016-05-03 12:30:16 +0530

    edited project

---
+++ swipedemo/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  swipedemo
+//
+//  Created by BharathiKumar on 03/05/16.
+//  Copyright © 2016 bharathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
